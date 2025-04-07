1340a5cd062a95b0cd437528f4e35e12a498c288
Gaurav <gauravkrdas19@gmail.com>
2025-04-07 23:16:24 +0530

    input useRef

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,26 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 import Buttons from './components/Buttons'
 
 const App = () => {
-  const [inp, setInp] = useState('')
-
-  function convertToNum(){
-    console.log(inp)
+  const inpRef = useRef(null);
+  
+  function convertToNum() {
+    const subInp = inpRef.current.value;
+    if(subInp !== '') console.log(subInp)
+    inpRef.current.value = ''
   }
 
   return (
     <div className='w-full h-screen bg-green-50 flex justify-center'>
-
-      <input
-        type="text"
-        placeholder="Enter number in words..."
-        className="w-lg p-3 h-14 my-8 border-3 rounded-md border-black text-black font-bold outline-none focus:outline-none hover:scale-[1.01] focus:scale-[1.01] cursor-pointer"
-        onChange={(e)=>setInp(e.target.value)}
-      />
-      <Buttons bgColor='bg-green-500' btnText='convert number' buttonCallback={convertToNum} />
-
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          placeholder="Enter number in words..."
+          className="w-lg p-3 h-14 my-8 border-3 rounded-md border-black text-black font-bold outline-none focus:outline-none hover:scale-[1.01] focus:scale-[1.01] cursor-pointer"
+          ref = {inpRef}
+        />
+        <Buttons bgColor='bg-green-500' btnText='convert number' buttonCallback={convertToNum} />
+      </form>
     </div>
   )
 }
