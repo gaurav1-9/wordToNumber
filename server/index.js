8df7418a4e84d0ad3632fc32ef25ffbd6e5c3064
Gaurav <gauravkrdas19@gmail.com>
2025-04-07 23:40:37 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const Joi = require('joi')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const wordToNum = require('./wordToNum')
 
 const app = express()
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors)
 
 function validateInput(request){
     const schema = Joi.object({
